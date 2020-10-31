6bf15df14f27a6d7b1e6541d4809a9f9e95b5512
demongodYY <yyuuqqii@outlook.com>
2020-10-31 22:57:26 +0800

    init react

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export interface HelloProps {}
+
+export const Hello = (props: HelloProps) => <h1>Hello from World!!!</h1>;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import {Hello} from './App';
+
+ReactDom.render(<Hello />, document.getElementById('root'));
